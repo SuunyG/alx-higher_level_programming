b382157807ac3f944a41616a211595e818052392
SuunyG <sundaygeorge247@gmail.com>
2022-10-27 05:38:22 -0700

    command line args: A program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
-    
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -11,6 +11,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
